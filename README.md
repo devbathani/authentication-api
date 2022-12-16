fd0a671479b8dbcecbb4d5978d37359911b8773d
devbathani <devbathani@credicxo.com>
2022-12-16 23:55:16 +0530

    Authentication api setup complete

---
+++ README.md
@@ -0,0 +1,2 @@
+# Authentication-Api
+This api is used for authentication using JWT tokens and mongoDb to save the data of users.
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const morgan = require('morgan')
+const createError = require('http-errors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', async(req, res, next) => 
+  {
+    res.send('Hello World!')
+  }  
+)
+
+app.use(async(req, res, next)=> {
+    ///Old Way 
+
+    // const error = new Error("Not Found!!!")
+    // error.status = 404
+    // next(error)
+
+    /// Package way
+    next(createError.NotFound())
+})
+
+app.use((err, req, res, next) =>{
+    res.status(err.status || 500) 
+    res.send({
+        error: {
+            status : err.status || 500,
+            message : err.message,
+        }
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
